0be1e214db6cb5ca10d4c5e09b563d8d4f43e834
npes-95 <npe.smith.95@gmail.com>
2021-08-22 16:13:01 +0200

    Initial commit.

---
+++ src/main.rs
@@ -0,0 +1,69 @@
+extern crate termion;
+
+mod tap;
+
+use crate::tap::Tap;
+use std::io::{stdin, stdout, Write};
+use std::time::Duration;
+use termion::event::Key;
+use termion::input::TermRead;
+use termion::raw::IntoRawMode;
+
+fn usage() {
+    print!("[q]: quit | [c]: clear tap count | [h]: this help")
+}
+
+fn main() {
+    let stdin = stdin();
+    let mut stdout = stdout().into_raw_mode().unwrap();
+    write!(
+        stdout,
+        "{}{}{}tap - bpm calculator",
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide,
+        termion::clear::All
+    )
+    .unwrap();
+
+    stdout.flush().unwrap();
+
+    let mut tap = Tap::new();
+
+    let reset_interval = Duration::from_secs(2);
+
+    for c in stdin.keys() {
+        write!(
+            stdout,
+            "{}{}{}",
+            termion::cursor::Goto(1, 1),
+            termion::cursor::Hide,
+            termion::clear::All
+        )
+        .unwrap();
+
+        match c.unwrap() {
+            Key::Char('q') => break,
+            Key::Char('h') => usage(),
+            Key::Char('c') => {
+                tap.reset();
+                print!("cleared count");
+                ()
+            }
+            _ => {
+                if tap.last_interval() > reset_interval {
+                    tap.reset();
+                    print!("cleared count");
+                } else {
+                    tap.tap();
+                    print!("count: {} -- bpm: {}", tap.count(), tap.bpm());
+                }
+
+                ()
+            }
+        }
+
+        stdout.flush().unwrap();
+    }
+
+    write!(stdout, "{}", termion::cursor::Show).unwrap();
+}
---
+++ src/tap.rs
@@ -0,0 +1,45 @@
+use std::time::{Duration, Instant};
+
+pub struct Tap {
+    count: u128,
+    cumulative_interval: Duration,
+    last_tap: Instant,
+}
+
+impl Tap {
+    pub fn new() -> Self {
+        Tap {
+            count: 0,
+            cumulative_interval: Duration::new(0, 0),
+            last_tap: Instant::now(),
+        }
+    }
+
+    pub fn reset(&mut self) {
+        self.count = 0;
+        self.cumulative_interval = Duration::new(0, 0);
+        self.last_tap = Instant::now();
+    }
+
+    pub fn tap(&mut self) {
+        self.count += 1;
+
+        if self.count > 0 {
+            self.cumulative_interval += self.last_tap.elapsed();
+        }
+
+        self.last_tap = Instant::now();
+    }
+
+    pub fn bpm(&self) -> u128 {
+        60000 / (self.cumulative_interval.as_millis() / self.count)
+    }
+
+    pub fn count(&self) -> u128 {
+        self.count
+    }
+
+    pub fn last_interval(&self) -> Duration {
+        self.last_tap.elapsed()
+    }
+}
